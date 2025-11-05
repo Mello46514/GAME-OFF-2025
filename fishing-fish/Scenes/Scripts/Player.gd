17d1ef63f1d3f903d919361fb16af03e1310285d
Mell0_64 <117129227+Mello46514@users.noreply.github.com>
2025-11-05 14:23:27 -0800

    PreAlpha 1.1
    Added the fishing hook, and got collision working.

--- fishing-fish/Scenes/Scripts/Player.gd
+++ fishing-fish/Scenes/Scripts/Player.gd
@@ -2,11 +2,12 @@ extends CharacterBody3D
 
 var motorRotation:float = 0.0
 
+var throwPower:float = 0.0
 var power:float = 0.0
 var input:Vector2 = Vector2()
 
 const MAXSpeed = 25.0
-const MAXFishingPower = 15.0
+const MAXFishingPower = 35.0
 
 # State Machine for Boater
 var state = 0
@@ -24,6 +25,9 @@ enum States {
 @onready var boat:Node3D = $Boat
 @onready var boatMotor:MeshInstance3D = $Boat/Motor
 @onready var Camera:Camera3D = $Boat/TestPlayer/Camera3D
+@onready var throwPoint:Node3D = $Boat/TestPlayer/Camera3D/ThrowPoint
+
+var fishingline:Object = null
 
 func _ready() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
@@ -34,7 +38,18 @@ func _physics_process(_delta: float) -> void:
 	input.x = Input.get_axis("left", "right")
 	input.y = Input.get_axis("up", "down")
 	
+	Global.PlayerFishingLinePos = throwPoint.global_position
 	
+	if Input.is_action_pressed("click"):
+		get_tree().queue_delete(fishingline)
+		fishingline = null
+		throwPower += 0.1
+	else:
+		throwPower = 0
+	if Input.is_action_just_released("click"):
+		throwLine()
+	
+	throwPower = clamp(throwPower,0.0,1.0)
 	
 	if state == States.Boating:
 		power -= input.y * 0.005
@@ -52,9 +67,15 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and not state == States.Struggling:
+	if event is InputEventMouseMotion and (not state == States.Struggling):
 		Camera.rotation_degrees.y -= event.relative.x
 		Camera.rotation_degrees.x -= event.relative.y
 		Camera.rotation_degrees.x = clamp(Camera.rotation_degrees.x,-90,90)
 
 func throwLine():
+	if fishingline == null:
+		var a := preload("res://Scenes/Common/hook.tscn").instantiate()
+		add_sibling(a)
+		fishingline = a
+		a.global_position = throwPoint.global_position
+		a.velocity = velocity + (Camera.global_position - throwPoint.global_position) * -50
--- fishing-fish/Scenes/Scripts/hook.gd
+++ fishing-fish/Scenes/Scripts/hook.gd
@@ -4,18 +4,30 @@ signal gotcha
 
 const gravity = 1
 
+var stuck = false
+
+@onready var line := $Line
+
 func _physics_process(_delta):
 	
-	velocity.y += gravity
+	if not stuck:
+		velocity.y -= gravity
+	else:
+		velocity = Vector3.ZERO
+	
+	line.points[0] = global_position
+	line.points[1] = Global.PlayerFishingLinePos
 	
 	
-	
-	move_and_slide() # Assuming your character moves here
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if collision.get_collider() is Area3D: # or RigidBody2D
-			var Enemy = collision.get_collider()
-			if Enemy.get_parent() != null:
-				if Enemy.get_parent().is_in_group("Enemy"):
-					Enemy.get_parent().link(gotcha, "_help()")
-					gotcha.emit()
+	if not is_on_floor() or is_on_wall():
+		move_and_slide() # Assuming your character moves here
+		
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			if collision.get_collider() is AnimatableBody3D: # or RigidBody2D
+				var Enemy = collision.get_collider()
+				if Enemy.get_parent() != null:
+					if Enemy.get_parent().is_in_group("Enemy"):
+						stuck = true
+						gotcha.connect(Enemy.get_parent()._help)
+						gotcha.emit()
